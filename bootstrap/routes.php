b6e25672021f12c0912e63f7adc94ec51919f4b8
Teng Keng Shen <kengshen92@gmail.com>
2017-10-14 18:10:43 +0800

    moved index.php
    moved index.php to root

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-$app->get('/home', '\App\Controllers\HomeController:index')->setName('home');
-$app->post('/home/shorten', '\App\Controllers\HomeController:shortenUrl')->setName('home.shorten');
+$app->get('/', '\App\Controllers\HomeController:index')->setName('home');
+$app->post('/shorten', '\App\Controllers\HomeController:shortenUrl')->setName('home.shorten');
 
 $app->get('/very/short/domain[/{id}]', '\App\Controllers\RedirectController:index')->setName('redirect');
