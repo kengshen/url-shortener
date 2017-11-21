0cf1c8cf29493c14ba278cbf56fa6fc1a72631d1
Teng Keng Shen <kengshen92@gmail.com>
2017-11-21 10:43:56 +0800

    remove optional
    argument of path to redirect should not be optional

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -3,4 +3,4 @@
 $app->get('/', '\App\Controllers\HomeController:index')->setName('home');
 $app->post('/shorten', '\App\Controllers\HomeController:shortenUrl')->setName('home.shorten');
 
-$app->get('/very/short/domain[/{id}]', '\App\Controllers\RedirectController:index')->setName('redirect');
+$app->get('/very/short/domain/{id}', '\App\Controllers\RedirectController:index')->setName('redirect');
